2c76cfae78bb58c4d2d7dc1573f3b8ad3686e138
Jonny Mälman <124621778+JonnyMalman@users.noreply.github.com>
2023-06-27 23:38:59 +0200

    Update entsoe_GetEnergyRate.lua

--- src/entsoe_GetEnergyRate.lua
+++ src/entsoe_GetEnergyRate.lua
@@ -1,11 +1,29 @@
 -- https://transparency.entsoe.eu/content/static_content/Static%20content/web%20api/Guide.html#_generation_domain
 -- 4.2.10. Day Ahead Rates [12.1.D]
 
-function QuickApp:getServiceRateData(callback, instance, fromdate, todate, reportError)
-    self:d("Request ENTSO-e for period UTC: " .. fromdate .. " -- " .. todate .. " (AreaCode: " .. self.areaCode .. ")")
+function QuickApp:getServiceRateData(callback, instance, date, exchRate, reportError)
+    -- ENTSO-e service only returns 24 hour Rates on each request even if we define another "to Date" :(
+    if (date == nil or date == "") then
+        self:d("Missing date when request ENTSO-e rate service")
+        return nil 
+    end
 
+    -- Don´t request ENTSO-e service if we already have the requested energy rates in table
+    if (self:existsInEnergyTariffTable(self.tariffAreaRates, date)) then
+        self:d("ENTSO-e rate already exists for date: " ..date .." (Exch: " ..tostring(exchRate) ..")")
+        self.serviceSuccess = true
+        return nil
+    end
+        
+    -- Set current Exchange rate if missing or 0 if nextday
+    if (exchRate == nil) then exchRate = self.exchangeRate end
+    if (tonumber(date) > tonumber(os.date("%Y%m%d"))) then exchRate = 0 end
+
+    -- Request ENTO-e service
+    self:d("Request ENTSO-e for period UTC: " ..date .."0000 -- " ..date .."2300 (AreaCode: " ..self.areaCode ..") Exch: " ..tostring(exchRate))
     local ratePrices = {}
-    local url = self.entsoe_baseURL .. "?documentType=A44&in_Domain=" .. self.areaCode .. "&out_Domain=" .. self.areaCode .. "&periodStart=" .. fromdate .. "&periodEnd=" .. todate .. "&securityToken=" .. self.token
+    local rateTable = {}
+    local url = self.entsoe_baseURL .."?documentType=A44&in_Domain=" ..self.areaCode .."&out_Domain=" ..self.areaCode .."&periodStart=" ..date .."0000&periodEnd=" ..date .."2300&securityToken=" ..self.token
 
     self.httpClient:request(url, {
         options = {
@@ -35,25 +53,28 @@ function QuickApp:getServiceRateData(callback, instance, fromdate, todate, repor
                 local startDate = self:getXmlDate(periodXml, "start", "%Y-%m-%d %H:%M")
                 local endDate = self:getXmlDate(periodXml, "end", "%Y-%m-%d %H:%M")
 
-                -- Create (UTC) date and rate table
+                -- Create local date and rate table
                 for index, rate in pairs(data) do
+                    local localDate = self:toLocalDate(startDate, self.timezoneOffset, "%Y%m%d")
                     local ratePrice = {
-                        id = self:toLocalDateId(startDate, index - 1, self.timezoneOffset),
+                        date = localDate,
+                        exch = exchRate,
                         rate = tonumber(rate)
                     }
                     table.insert(ratePrices, ratePrice)
                 end
 
-                self:d("=> Response: Start UTC = " .. startDate .. ", End UTC = " .. endDate .. ", " .. self:tableCount(ratePrices) .. " rates")
-                
+                self.serviceRequestTime = os.date(self:getDateFormat()) .." " ..os.date("%H:%M")
                 self.serviceSuccess = true
                 self.serviceMessage = ""
-
+                self.dataChanged = true
+                self:d("=> ENTSO-e Success response: Start UTC = " .. startDate .. ", End UTC = " .. endDate .. ", " .. self:tableCount(ratePrices) .. " rates")
+                
                 pcall(callback, instance, ratePrices)
             else
                 if (reportError == true) then
                     self.serviceSuccess = false
-                    self.serviceMessage = "Error: Can't get energy prices from ENTSO-e"
+                    self.serviceMessage = "Error: Can't get energy rates from ENTSO-e for area: " ..self.areaName
                     self:debug(self.serviceMessage)
                 end
                 return nil
@@ -62,10 +83,32 @@ function QuickApp:getServiceRateData(callback, instance, fromdate, todate, repor
         error = function(message)
             if (reportError == true) then
                 self.serviceSuccess = false
-                self.serviceMessage = "Error: " ..message
+                self.serviceMessage = "ENTSO-e Error: " ..message
                 self:debug(self.serviceMessage)
+                self.httpClient = net.HTTPClient()
             end
             return nil
         end
     })
+end
+
+-- Extract ENTSO-e prices from response xml into Lua table
+function QuickApp:xml2PriceTable(xml)
+    local priceTable = {}
+    local ni, c, label, xarg, empty
+    local i, j = 1, 1
+
+    while true do
+        ni, j, c, label, xarg, empty = string.find(xml, "<(%/?)([%w:_]+)(.-)(%/?)>", i)
+        if not ni then break end
+        local text = string.sub(xml, i, ni-1)
+   
+        if not string.find(text, "^%s*$") and label == "price" then
+            table.insert(priceTable, text)
+        end
+
+        i = j+1
+    end
+
+    return priceTable
 end
