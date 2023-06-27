bb72d8408647cea6138a02869038ba66cae99233
Jonny Mälman <124621778+JonnyMalman@users.noreply.github.com>
2023-06-27 23:40:32 +0200

    Update fibaroTariffRate.lua

--- src/fibaroTariffRate.lua
+++ src/fibaroTariffRate.lua
@@ -1,47 +1,55 @@
 -- Update FIBARO Tariff rate table
 function QuickApp:updateFibaroTariffTable()
     -- Exit if no data in global table or we got Service error
-    if (self:getGlobalFibaroVariable(self.global_var_fibaro_tariff_name, "ON") == "OFF" or self:tableCount(self.tariffData[self.areaName]) == 0 or self.serviceSuccess == false) then return end
+    if (self:getGlobalFibaroVariable(self.global_var_fibaro_tariff_name, "ON") == "OFF" or self:tableCount(self.tariffAreaRates) == 0) then return end
 
     -- Get current FIBARO Energy Tariff rate data
     local tariffData = api.get("/energy/billing/tariff")
     local currRate = tariffData.rate
+    local exchRate = self.exchangeRate
     local addTariffs = {}
-    local currentRateTime = os.date("%y%m%d%H")
+    local currentRateTime = os.date("%Y%m%d%H")
     local count = 0
 
     -- Update FIBARO Additional Tariff table in local currency/kWh and local timezone
-    for index, tariff in pairs(self.tariffData[self.areaName]) do
-        local startTime = self:toDate(tariff.id, "%H:%M", 0)
-        local endTime = self:toDate(tariff.id, "%H:%M", 1)
-        local tariffName = self:toDate(tariff.id, self:getDateFormat(), 0) .." " ..startTime .." (" ..tariff.rate .." €/MWh)"
+    for _, dateTariff in pairs(self.tariffAreaRates) do
+        if (dateTariff == nil) then return end
+        exchRate = dateTariff.exch
 
-        -- FIBARO only display price in kWh
-        local locRate = self:getLocalTariffRate(tariff.rate, self.exchangeRate, "kWh", self.tax, self.operatorCost, self.gridLosses, self.gridAdjustment, self.dealerCost, self.gridCost)
+        for index, tariff in pairs(dateTariff.rates) do
+            local time = index - 1
+            local currTime = self:toDate(dateTariff.date, time, "%Y%m%d%H")
+            local startTime = self:toDate(dateTariff.date, time, "%H:%M", 0)
+            local endTime = self:toDate(dateTariff.date, time, "%H:%M", 1)
+            local tariffName = self:toDate(dateTariff.date, time, self:getDateFormat(), 0) .." " ..startTime .." (" ..tariff[1] .." €/MWh)"
 
-        -- FIBARO Tariff table can't have negative values :(
-        if (locRate <= 0) then
-            tariffName = tariffName .." " ..string.format(self:getValueFormat(), locRate) .." ⛔"
-            locRate = 0.00001
+            -- Calculate to Local Tariff Rate price, FIBARO can only display prices in kWh
+            local locRate = self:calculateTariffRate(tariff[1], exchRate, "kWh", self.tax, self.operatorCost, self.gridLosses, self.gridAdjustment, self.dealerCost, self.gridCost)
+
+            -- FIBARO Tariff table can't have zero or negative value :(
+            if (locRate <= 0) then
+                tariffName = tariffName .." " ..string.format(self:getValueFormat(), locRate) .." ⛔"
+                locRate = 0.00001
+            end
+
+            -- Get current rate
+            if (currTime == currentRateTime) then 
+                currRate = locRate
+                tariffName = tariffName .." ⭐"
+                self:d("Current energy price: "..tariff[1] .."=" ..currRate .." at exchange: " ..dateTariff.exch ..", Unit: " ..self.unit ..", Name: " ..tariffName)
+            end
+            
+            -- Add additional tariff to local tariff table
+            local newTariff = {
+                name = tariffName,
+                rate = locRate,
+                startTime = startTime,
+                endTime = endTime,
+                days = {string.lower(self:toDate(dateTariff.date, time, "%A", 0))}
+            }
+            table.insert(addTariffs, newTariff)
+            count = count + 1
         end
-
-        -- Get current rate
-        if (tariff.id == currentRateTime) then 
-            currRate = locRate
-            tariffName = tariffName .." ⭐"
-            self:d("Current energy price: "..tariff.rate .."=" ..currRate .." at exchange: " ..self.exchangeRate .." Unit: " ..self.unit .." ID: " ..tariff.id .." - " ..tariffName)
-        end
-        
-        -- Add additional tariff to local tariff table
-        local newTariff = {
-            name = tariffName,
-            rate = locRate,
-            startTime = startTime,
-            endTime = endTime,
-            days = {string.lower(self:toDate(tariff.id, "%A", 0))}
-        }
-        table.insert(addTariffs, newTariff)
-        count = count + 1
     end
 
     -- Save new tariff table to FIBARO Tariff table
