7187ff094b78be56c9aae0be3b103c415e4bcd94
Jonny Mälman <124621778+JonnyMalman@users.noreply.github.com>
2023-10-19 19:54:30 +0200

    Update defaults.lua

--- src/defaults.lua
+++ src/defaults.lua
@@ -11,6 +11,16 @@ function QuickApp:createGlobalTableVariable()
 end
 
 function QuickApp:createGlobalVariables()
+    -- Create global variable Current energy rate price
+    local rate_var = {
+            name=self.global_var_current_rate_name,
+            isEnum=false,
+            readOnly=true,
+            value=""
+        }
+    response, status = api.post('/globalVariables/', rate_var)
+    self:d("Create global variable: " ..tostring(rate_var.name) .." => " ..tostring(status) .." - " ..tostring(response.message))
+
     -- Create Energy store in FIBARO Tariff global variable
     local tariff_var = {
             name=self.global_var_fibaro_tariff_name,
@@ -39,7 +49,7 @@ function QuickApp:createGlobalVariables()
             isEnum=true,
             readOnly=true,
             value="HIGH",
-            enumValues={"VeryLOW","LOW","MEDIUM","HIGH","VeryHIGH"}
+            enumValues={"Negative","VeryLOW","LOW","MEDIUM","HIGH","VeryHIGH"}
         }
     response, status = api.post('/globalVariables/', level_var)
     self:d("Create global variable: " ..tostring(level_var.name) .." => " ..tostring(status) .." - " ..tostring(response.message))
@@ -53,8 +63,6 @@ function QuickApp:createGlobalVariables()
     level_var.name = self.global_var_month_level_name
     response, status = api.post('/globalVariables/', level_var)
     self:d("Create global variable: " ..tostring(level_var.name) .." => " ..tostring(status) .." - " ..tostring(response.message))
-
-    self:d("FIBARO Global variables created.")
 end
 
 function QuickApp:setLocalVariables()
@@ -63,6 +71,12 @@ function QuickApp:setLocalVariables()
     -- How to get an Token: https://transparency.entsoe.eu/content/static_content/download?path=/Static%20content/API-Token-Management.pdf 
     self.token = self:getLocalVariable(self.variable_token_name, self.default_token)
 
+    -- Exchange rate access key is required if use of other currency than Euro.
+    -- Register a free Exchangerate.host account at: https://exchangerate.host and get your Access Key.
+    self.exchangerate_Key = self:getLocalVariable(self.var_exchange_rate_Key_name, "")
+    self.exchangeLastUpdate = self:getLocalVariable(self.var_exchange_last_update_name, "")
+    self.exchangeRate = tonumber(self:getLocalVariable(self.var_exchange_rate_name, 0))
+    
     -- Get/Set local rate price variables
     self.low_price = tonumber(self:getLocalVariable(self.variable_Low_name, self.default_Low_price))
     self.medium_price = tonumber(self:getLocalVariable(self.variable_Medium_name, self.default_Medium_price))
@@ -89,6 +103,9 @@ function QuickApp:setLocalVariables()
     -- Get/Set local variable grid cost
     self.gridCost = self:getLocalVariable(self.variable_grid_cost_name, self.default_grid_cost)
 
+    -- Get/Set local variable allow negative rates
+    self.allowNegative = self:toBool(self:getLocalVariable(self.variable_allow_negative_name, self.default_allow_negative))
+
     -- Get/Set global FIBARO variables
     self.areaName = self:getGlobalFibaroVariable(self.global_var_area_name, self.default_area_name)
     self.areaCode = self:getAreaCode(self.areaName)
@@ -104,10 +121,7 @@ function QuickApp:refreshDisplayVariables()
     self.timezoneOffset = tonumber(fibaroSettings.timezoneOffset)
     self.dateFormat = fibaroSettings.dateFormat
     self.i18n = i18n:new(fibaroSettings.defaultLanguage)
-    
-    -- If Euro set exchange rate to 1:1
-    if (self.currency == "EUR") then self.exchangeRate = 1 end
-    
+        
     -- Refrech global variables
     self.areaName = fibaro.getGlobalVariable(self.global_var_area_name)
     self.areaCode = self:getAreaCode(self.areaName)
@@ -118,22 +132,24 @@ function QuickApp:refreshDisplayVariables()
     self:loadEnergyTariffTable()
 
     -- Refresh QA variable values
-    self.token = self:getVariable(self.variable_token_name)
-    self.tariffHistory = tonumber(self:getVariable(self.variable_tariff_history_name))
-    self.low_price = tonumber(self:getVariable(self.variable_Low_name))
-    self.high_price = tonumber(self:getVariable(self.variable_High_name))
-    self.veryhigh_price = tonumber(self:getVariable(self.variable_VeryHigh_name))
-    self.tax = tonumber(self:getVariable(self.variable_tax_percentage_name))
-    self.decimals = self:getVariable(self.variable_price_decimals_name)
+    self.token = self:getVariable(self.var_token_name)
+    self.tariffHistory = tonumber(self:getVariable(self.var_tariff_history_name))
+    self.low_price = tonumber(self:getVariable(self.var_Low_name))
+    self.high_price = tonumber(self:getVariable(self.var_High_name))
+    self.veryhigh_price = tonumber(self:getVariable(self.var_VeryHigh_name))
+    self.tax = tonumber(self:getVariable(self.var_tax_percentage_name))
+    self.decimals = self:getVariable(self.var_price_decimals_name)
     self.valueFormat = self:getValueFormat()
 
-    self.operatorCost = tonumber(self:getVariable(self.variable_operator_cost_name))
-    self.gridLosses = tonumber(self:getVariable(self.variable_grid_losses_name))
-    self.gridAdjustment = tonumber(self:getVariable(self.variable_grid_adjustment_name))
-    self.dealerCost = tonumber(self:getVariable(self.variable_dealer_cost_name))
-    self.gridCost = tonumber(self:getVariable(self.variable_grid_cost_name))
+    self.operatorCost = tonumber(self:getVariable(self.var_operator_cost_name))
+    self.gridLosses = tonumber(self:getVariable(self.var_grid_losses_name))
+    self.gridAdjustment = tonumber(self:getVariable(self.var_grid_adjustment_name))
+    self.dealerCost = tonumber(self:getVariable(self.var_dealer_cost_name))
+    self.gridCost = tonumber(self:getVariable(self.var_grid_cost_name))
 
-    self.addTariffDate = self:getVariable(self.variable_add_date_tariff_name)
+    self.allowNegative = self:toBool(self:getVariable(self.var_allow_negative_name))
+
+    self.addTariffDate = self:getVariable(self.var_add_date_tariff_name)
 
     -- Update QA Unit property
     self:updateProperty("unit", self:getCurrencySymbol() .. "/" ..tostring(self.unit))
@@ -145,6 +161,7 @@ end
 -- Get local QA variable, set to default value if variable is missing
 function QuickApp:getLocalVariable(name, defaultValue)
     if defaultValue == nil then defaultValue = "" end
+    if (name == nil) then return defaultValue end
     local value = self:getVariable(name)
 
     if (value == nil or value == "" or value == "nil") then
@@ -158,6 +175,7 @@ end
 -- Get local QA variable, set to default value if variable is missing
 function QuickApp:getGlobalFibaroVariable(name, defaultValue)
     if defaultValue == nil then defaultValue = "" end
+    if (name == nil) then return defaultValue end
     local value = fibaro.getGlobalVariable(name)
 
     if (value == nil or value == "" or value == "nil") then
