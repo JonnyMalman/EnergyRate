cc58502b0105001ef41ae4d386fd14db9534235e
Jonny Mälman <124621778+JonnyMalman@users.noreply.github.com>
2023-10-19 19:53:19 +0200

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,18 +1,42 @@
 --[[
+    =======================================================================================================    
+        MIT License
+
+        Copyright © 2023 JAM Data® AB by Jonny Mälman
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.    
+    =======================================================================================================    
+
     ENTSO-e Energy Rate is an FIBARO QuickApp that get current Spot prices for almost every european countries and is independent from any energy power company for free.
 
-    How to get your own Token:
+    How to get your own ENTSO-e Token:
     I´ve provide an Token that works "out of the box", but can be changed in the future, so if you like you can create your own free token at ENTSO-e, but not required.
     Register an ENTSO-e account at: https://transparency.entsoe.eu/
     How to get an Token: https://transparency.entsoe.eu/content/static_content/download?path=/Static%20content/API-Token-Management.pdf
 
-    The Exchange rate service (https://exchangerate.host) that is used in this QuickApp to get your local currency is also free to use and if you like it, donate a cappuccino at https://exchangerate.host/#/donate ;)
+    The Exchange rate service (https://exchangerate.host) that is used in this QuickApp gets your local currency for free up to 1000 request/month if you register a account.
+    If you use other currency than EUR, you need create and add the API Access Key from https://exchangerate.host in the local variables [ExchAccessKey].
 
     This is the first time I have developed in Lua language, so have some indulgence, but hey it works and I hope it works for U2 ;)
     I would appreciate if you have any feedback or suggestions to make this QuickApp better or more usefull, please send me an email at energyrate@jamdata.com and I´ll try to do my best :)
 
     And please give this QA your rating ⭐⭐⭐⭐⭐ in FIBARO Marketplace if you like it or not, I'll would be very happy!
-
 
     CHANGELOG:
     v1.0 First release 2023-01
@@ -44,32 +68,44 @@
         - Add translation in Portuguese (Thanks to Leandro C.).
         - Add local QA cost variables to calculate energy prices: {((ENTSO_price + operatorCost) x losses x adjustment) + dealer + localgrid} x tax (by Leandro C.).
 
-    v1.4 bug fix release 2023-05
+    v1.4 Bug fix release 2023-05
         - Fix Update timer for display panel and variables. 
 
-    v1.5 fix Exchange rate 2023-06
+    v1.5 Fix Exchange rate 2023-06
         - Fix historical exchange rates when show in FIBARO Tariff table.
         - Add QA varible [AddTariffDate] if you want to add historical rates to energy table. Input format: "YYYY-MM-DD".
 
         Braking changes from v1.4:
             All new tariff rates will now be stored in a new general variable [EnergyTariffTable] and all you old data will remain in [EnergyStateTable] until you delete it.
 
-    v1.6 Fix QA Child value display 2023-07
+    v1.6 Fix QA Child value display 2023-08
         - Corrected QA Child to show negative values.
-        - Add Check for new QA update button. (Beta)
+        - Add local QA variable [NegativeRates] to allow negative rates ON/OFF
+        - Add new rate level if negative rate 🟣
+        - Add general variable [EnergyCurrentRate] to easier read current energy rate price from other QA or scenes.
+
+    v1.7 Fix Exchange rate API 2023-10
+        - Update to support the new Exchangerate.Host API.
+          This release Requires API Access Key from https://exchangerate.host only if you use other currency than Euro €.
+          (NOTE! Historical exchange rates can't be fetch with this release because of changes at Exchangerate.Host)
+
+    v1.7.1 Display panel fix 2023-10
+        - Display panel information fix.
+    
 ]]
 
 -- QuickApp Initialize
 function QuickApp:onInit()
-    self.debugOn = false  -- Write to debug console true/false
+    self.debugLog = false   -- Write to debug console true/false. Is set by local variable [ConsoleDebug]=ON/OFF.
     self.deviceName = "ENTSO-e Energy Rate"
-    self.version = "1.6"
+    self.version = "1.7.1"
+    self.Copyright = "Copyright © 2023 JAM Data® AB"
 
     self.httpClient = net.HTTPClient()
     
-    -- Variables for exchangerate.host Api service
-    -- https://exchangerate.host
-    self.exchangerate_baseURL = "https://api.exchangerate.host/"
+    -- Variables for exchangerate.host Api service if use of other currency than Euro €.
+    -- https://exchangerate.host  -- Free account now only support for non secure http and max 1000 requests/month. No history exchange rate can requests on a free account!
+    self.exchangerate_baseURL = "http://api.exchangerate.host/"
     
     -- Variables for ENTSO-e Transparency Platform Api service
     -- https://transparency.entsoe.eu/content/static_content/Static%20content/web%20api/Guide.html
@@ -78,36 +114,42 @@ function QuickApp:onInit()
     self.next_rank_device_id = nil                                -- Id of next energy price QA child
 
     -- NOTE! Local QA variable names can only have maximun of 15 charachters.
-    self.variable_token_name = "ENTSOE_Token"                     -- Variable name of ENTSO-e Token key
-    self.variable_tariff_history_name = "TariffHistory"           -- Variable name of tariff day(s) history
-    self.variable_Low_name = "PriceLow"                           -- Variable name of low price
-    self.variable_Medium_name = "PriceMedium"                     -- Variable name of medium price.
-    self.variable_High_name = "PriceHigh"                         -- Variable name of high price.
-    self.variable_VeryHigh_name = "PriceVeryHigh"                 -- Variable name of very high price.
-    self.variable_tax_percentage_name = "EnergyTax"               -- Variable name of energy tax.
-    self.variable_price_decimals_name = "PriceDecimals"           -- Variable name of number of decimals when display energy prices.
-    self.variable_add_date_tariff_name = "AddTariffDate"          -- Variable name of date to add historical tariff rate.
-    self.variable_operator_cost_name = "OperatorCost"             -- Variable name of Grid Operator deviation costs (€/kWh or €/MWh)
-    self.variable_grid_losses_name = "GridLossesPct"              -- Variable name of Grid Losses percent cost (%).
-    self.variable_grid_adjustment_name = "AdjustmentPct"          -- Variable name of Grid Adjustment percent cost added to the Grid Losses (%)
-    self.variable_dealer_cost_name = "DealerFee"                  -- Variable name of Dealer fee cost (€/KWh or €/MWh)
-    self.variable_grid_cost_name = "LocalGridCost"                -- Variable name of Local Grid cost (€/KWh or €/MWh)
+    self.var_debug_log_name = "ConsoleDebug"                      -- Variable name of Debug log to console On/Off.
+    self.var_token_name = "ENTSOE_Token"                          -- Variable name of ENTSO-e Token key.
+    self.var_tariff_history_name = "TariffHistory"                -- Variable name of tariff day(s) history.
+    self.var_Low_name = "PriceLow"                                -- Variable name of low price.
+    self.var_Medium_name = "PriceMedium"                          -- Variable name of medium price.
+    self.var_High_name = "PriceHigh"                              -- Variable name of high price.
+    self.var_VeryHigh_name = "PriceVeryHigh"                      -- Variable name of very high price.
+    self.var_tax_percentage_name = "EnergyTax"                    -- Variable name of energy tax.
+    self.var_price_decimals_name = "PriceDecimals"                -- Variable name of number of decimals when display energy prices.
+    self.var_add_date_tariff_name = "AddTariffDate"               -- Variable name of date to add historical tariff rate.
+    self.var_operator_cost_name = "OperatorCost"                  -- Variable name of Grid Operator deviation costs (€/kWh or €/MWh)
+    self.var_grid_losses_name = "GridLossesPct"                   -- Variable name of Grid Losses percent cost (%).
+    self.var_grid_adjustment_name = "AdjustmentPct"               -- Variable name of Grid Adjustment percent cost added to the Grid Losses (%).
+    self.var_dealer_cost_name = "DealerFee"                       -- Variable name of Dealer fee cost (€/kWh or €/MWh).
+    self.var_grid_cost_name = "LocalGridCost"                     -- Variable name of Local Grid cost (€/kWh or €/MWh).
+    self.var_allow_negative_name = "NegativeRates"                -- Variable name of Allow negative rates ON/OFF.
+    self.var_exchange_last_update_name = "ExchLastUpdate"         -- Variable name of Laste date exchange rate update.
+    self.var_exchange_rate_name = "ExchangeRate"                  -- Variable name of current exchange rate.
+    self.var_exchange_rate_Key_name = "ExchAccessKey"             -- Variable name of Exchangerate.host API Access Key (Required only if your currency is other than € Euro).
 
     -- Global variable names
-    self.global_var_unit_name = "EnergyUnit"                      -- Global variable name of energy unit (KWh or MWh)
-    self.global_var_state_table_name = "EnergyTariffTable"        -- Global variable name of energy tariff table
-    self.global_var_area_name = "EnergyArea"                      -- Global variable name of energy area
-    self.global_var_level_name = "EnergyHourLevel"                -- Global variable name of energy level
-    self.global_var_next_level_name = "EnergyNextHourLevel"       -- Global variable name of energy level
-    self.global_var_month_level_name = "EnergyMonthLevel"         -- Global variable name of energy level
-    self.global_var_fibaro_tariff_name = "EnergyTariffInFibaro"   -- Global variable name of show Tariff rates i FIBARO On/Off
-    
+    self.global_var_unit_name = "EnergyUnit"                      -- Global variable name of energy unit (KWh or MWh).
+    self.global_var_state_table_name = "EnergyTariffTable"        -- Global variable name of energy tariff table.
+    self.global_var_area_name = "EnergyArea"                      -- Global variable name of energy area.
+    self.global_var_level_name = "EnergyHourLevel"                -- Global variable name of energy level.
+    self.global_var_next_level_name = "EnergyNextHourLevel"       -- Global variable name of energy level.
+    self.global_var_month_level_name = "EnergyMonthLevel"         -- Global variable name of energy level.
+    self.global_var_fibaro_tariff_name = "EnergyTariffInFibaro"   -- Global variable name of show Tariff rates i FIBARO On/Off.
+    self.global_var_current_rate_name = "EnergyCurrentRate"       -- Global variable name of current energy rate price.
+            
     -- Default values to set if missing
     self.default_token = "f442d0b3-450b-46d7-b752-d8d692fdb2c8"   -- See "How to get your own Token:" above.
     self.default_area_name = "[SELECT AREA]"                      -- Need to select an area before request ENTSO-e.
     self.default_unit = "kWh"                                     -- Show kWh or MWh in display panel (FIBARO Tariff table is always in kWh)
     self.default_tax = "0"                                        -- Default 0% energy tax.
-    self.default_tariff_history = "62"                            -- Default 62 days ~2 month.
+    self.default_tariff_history = "62"                            -- Default 62 days ~2 month. (FIBARO Tariff panel will be slow to open if it's too many days)
 
     self.default_decimals = self:getDefaultPriceDecimals()        -- Get default number of decimals based on currency for price display.
     self.default_Low_price = self:getDefaultRatePrice(10)         -- Set 10% of medium price based on local currency.
@@ -120,23 +162,22 @@ function QuickApp:onInit()
     self.default_grid_adjustment = "0"                            -- Default adjustment added to the Grid Losses in percent (0 %).
     self.default_dealer_cost = "0"                                -- Default Dealer cost (0 €/KWh or 0 €/MWh).
     self.default_grid_cost = "0"                                  -- Default Local Grid cost (0 €/KWh or 0 €/MWh).
+    self.default_allow_negative = "ON"                            -- Default Allow negative rates.
 
     -- Fixed QA variables
     self.nextday_releaseHourUtc = 12                              -- The UTC hour of the day when ENTSO-e releses the next day prices.
     self.dateFormat = self:getDateFormat()                        -- The FIBARO Date format setting ("YY-MM-DD", "DD.MM.YY" or "MM/DD/YY").
     self.valueFormat = self:getValueFormat(self.default_decimals) -- The value formatting of decimals when display Energy prices ie. "%.3f"
     self.serviceRequestTime = "--"                                -- Last datetime when we request ENTSO-e webservice.
-    self.serviceSuccess = false                                   -- Request ENTSO-e service success or fault.
+    self.serviceSuccess = true                                   -- Request ENTSO-e service success or fault.
     self.exchangeRateUpdated = false                              -- Status if Exchange Rate is updated or not.
-    self.exchangeRateLastDate = ""                                -- Last datetime when we request Exchange Rate Api service.
-    self.exchangeRate = 0                                         -- Set default excahnge rate 0
-    self.serviceMessage = ""                                      -- Request ENTSO-e service error message.
+    self.exchangeLastDate = ""                                    -- Last datetime when we request Exchange Rate Api service.
+    self.exchangeRate = 0                                         -- Set default excahnge rate 0.
+    self.entsoeServiceMessage = ""                                -- Request ENTSO-e service error message.
+    self.exchServiceMessage = ""                                  -- Request Exchange rate service error message.
     self.lastVariableUpdate = "--"                                -- Last general variable update.
-    self.addTariffDate = ""                                       -- Add historical tariff rates from a date (YYYY-MM-DD).
-    self.dataChanged = true                                       -- If data has change and display panel need to be updated 
-    self.lastExchange = nil                                       -- Last exchange rate value
-    self.fibaroQaVersion = ""                                     -- QA Version on FIBARO Marketplace
-    self.checkQAUpdateDays = 7                                    -- Check for new QA version in days on FIBARO Marketplace
+    self.addTariffDate = ""                                       -- Add historical tariff rates for a date (YYYY-MM-DD).
+    self.dataChanged = true                                       -- If data has change then display panel need to be updated.
 
     -- Let´s start the engine
     self:mainStart()
@@ -144,7 +185,8 @@ end
 
 -- QA Main start execution
 function QuickApp:mainStart()
-    self:debug(">>>> Start QuickApp " ..self.deviceName .." v" ..self.version .." <<<<")
+    self.debugLog = self:toBool(self:getLocalVariable(self.var_debug_log_name, "OFF"))
+    self:debug(">>>> Start QuickApp " ..self.deviceName .." v" ..self.version .." - " ..self.Copyright .." <<<<")
     
     -- Create global varaiables (See: defaults)
     self:createAreaVariables()    
@@ -165,28 +207,20 @@ end
 function QuickApp:mainLoop(init)
     local loopTime = 2000
        
-    -- Get current Exchange rate from Exchangerate.host Api Service if local currency is other than Euro
-    if (self.currency == "EUR") then 
-        self.exchangeRate = 1           -- Set default excahnge rate 1:1 for €
-        self.exchangeRateUpdated = true
-    else 
-        self:getServiceExchangeData(QuickApp.setExchangeRate, self)
-    end
+    -- Get current Exchange rate from Exchangerate.host Api Service if local currency is other than € Euro
+    local dataChanged = self:getServiceExchangeRate(init)
 
     if (init == false) then
         -- Update Energy Rates table from ENSO-e Service if AreaCode is set
         if (self.areaCode ~= "") then self:refreshEnergyTariffTable() end
         
         -- Update variable and display panel if next hour
-        if (not self:isDisplayPanelUpToDate()) then
+        if (not self:isDisplayPanelUpToDate(dataChanged)) then        
             fibaro.setTimeout(2000, function()
                 self:refreshDisplayVariables() -- Update Variables and Display panel
                 self:updateFibaroTariffTable() -- Update FIBARO Tariff table if ON
             end)
         end
-
-        -- Check FIBARO Marketplace if QA update exists
-        pcall(function () self:checkQAUpdates(false) end)
 
         -- Set to loop to each minute
         loopTime = 60000 - (os.date("%S") * 1000)
@@ -203,12 +237,7 @@ function QuickApp:refresh_action()
     
     -- Update Tariff table, variables and panel
     self.dataChanged = true
+    self:getServiceExchangeRate(true)
     self:refreshEnergyTariffTable()
     fibaro.setTimeout(5000, function() self:refreshDisplayVariables() end)    
-end
-
--- Trigger if panel "Check for Update" button pressed
-function QuickApp:checkUpdate_action()
-    pcall(function () self:checkQAUpdates(true) end)
-    fibaro.setTimeout(2000, function() self:refreshDisplayVariables() end)   
 end
